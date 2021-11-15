13c14522451b9b68a4a62987c6e41e68c5664210
ChoiYeEun <89172314+rosy-y@users.noreply.github.com>
2021-11-15 13:37:29 +0900

    Create LV2_1.cpp
    N개의 최소공배수

---
+++ LV2_1.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int GCD(int a, int b) {
+    if (a%b == 0) return b;
+    else return GCD(b, a%b);
+}
+
+int LCM(int a, int b) {
+    return a * b / GCD(a,b);
+}
+
+int solution(vector<int> arr) {
+    int answer = arr[0];
+    
+    for (int i=1; i<arr.size(); i++) {
+        answer = LCM(answer, arr[i]);
+    }
+    
+    return answer;
+}
