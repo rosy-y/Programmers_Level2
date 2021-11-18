fe14a7a4941eae665de300e9a722fd8499b53f61
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2021-11-18 15:52:29 +0900

    Create Solution.cpp
    8> 땅 따먹기

---
+++ 땅따먹기/Solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<vector<int>> land) {
+    int answer = 0;
+    int line = land.size();
+    
+    for (int i=0; i<line-1; i++) {
+        land[i+1][0] += max(land[i][1], max(land[i][2], land[i][3]));
+        land[i+1][1] += max(land[i][0], max(land[i][2], land[i][3]));
+        land[i+1][2] += max(land[i][0], max(land[i][1], land[i][3]));
+        land[i+1][3] += max(land[i][0], max(land[i][1], land[i][2]));
+    }
+    
+    answer = *max_element(land[line-1].begin(), land[line-1].end());
+    return answer;
+}
