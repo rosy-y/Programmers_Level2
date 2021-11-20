004e1dc0f2f595bc64fdf9cdec7554ef114b8110
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2021-11-20 16:47:02 +0900

    Create Solution.cpp
    11> 올바른 괄호

---
+++ 올바른 괄호/Solution.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    int flag = 0;
+    
+    for(int i=0; i<s.size(); i++) {
+        if (s[i] == '(') flag++;
+        else flag--;
+        
+        if (flag < 0) answer = false;
+    }
+    
+    if(flag != 0) answer = false;
+    return answer;
+}
