a2df39657099a2f5398a6a1bbe10620c73744ca9
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2021-11-20 16:47:44 +0900

    Create Solution.cpp
    12> 파일명 정렬

---
+++ 파일명 정렬/Solution.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+using namespace std;
+
+int numIdx(const string& str){
+    for (int i=0; i<str.size(); i++){
+        if(str[i]>='0' && str[i]<='9')
+            return i;
+    }
+}
+
+char alpha(char c) {
+    if ('a'<=c && c<='z') c -= 32;
+        return c;
+}
+
+string getHead(string str){
+    string tmp = str.substr(0, numIdx(str));
+    transform(tmp.begin(), tmp.end(), tmp.begin(), alpha);
+    return tmp;
+}
+
+int getNum(string str) {
+    return stoi(str.substr(numIdx(str)));
+}
+
+bool cmpNum(string a, string b) {
+    return getNum(a) < getNum(b);
+}
+bool cmpHead(string a, string b) {
+    return getHead(a) < getHead(b);
+}
+
+vector<string> solution(vector<string> files) {    
+    stable_sort(files.begin(), files.end(), cmpNum);
+    stable_sort(files.begin(), files.end(), cmpHead);
+    return files;
+}
