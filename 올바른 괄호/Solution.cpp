495a1feb557ec92cb22a2d75a8cc007f2891a900
YeEun_Choi <89172314+rosy-y@users.noreply.github.com>
2022-03-15 21:40:52 +0900

    Update Solution.cpp

--- 올바른 괄호/Solution.cpp
+++ 올바른 괄호/Solution.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 bool solution(string s) {
     bool answer = true;
-    int flag = 0;
+    int flag = 0;  // check
     
     for(int i=0; i<s.size(); i++) {
         if (s[i] == '(') flag++;
